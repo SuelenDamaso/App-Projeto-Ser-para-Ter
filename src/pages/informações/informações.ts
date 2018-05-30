5799559bb45614d9a34fcd29cf751ce2aa919c32
SuelenDamaso <suelen.pos_vendas.jrp@hotmail.com>
2018-05-30 17:08:31 -0300

    pagina colaborador

--- src/pages/informações/informações.ts
+++ src/pages/informações/informações.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { ColaboradorPage } from '../colaborador/colaborador';
 
 @IonicPage()
 @Component({
@@ -16,6 +17,8 @@ export class InformaçõesPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad InformaçõesPage');
   }
-  
+  goToColaborar(){
+    this.navCtrl.push(ColaboradorPage)
+  }
 
 }
