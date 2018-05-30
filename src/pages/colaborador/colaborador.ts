86263258c2adb6edcb3cc5622d85d87a8f96f5b4
SuelenDamaso <suelen.pos_vendas.jrp@hotmail.com>
2018-05-30 16:59:38 -0300

    quarto

--- src/pages/colaborador/colaborador.ts
+++ src/pages/colaborador/colaborador.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AlertController } from 'ionic-angular';
+
 /**
  * Generated class for the ColaboradorPage page.
  *
@@ -15,8 +17,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ColaboradorPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController) {
   }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Conta para depósito:',
+      subTitle: 'Banco Bradesco -----------------Agencia: 1614, C/C: 0505405-2   Agradeçemos pela sua doação!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ColaboradorPage');
